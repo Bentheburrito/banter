420cd146f43c56ebbb2340b8e45aeb0383c651ab
Ben <lolitsaburrito@gmail.com>
2022-07-08 20:27:15 -0400

    users now have usernames, and need one to register

--- lib/banter/accounts/user.ex
+++ lib/banter/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Banter.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
@@ -30,8 +31,9 @@ defmodule Banter.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :username, :password])
     |> validate_email()
+    |> validate_username()
     |> validate_password(opts)
   end
 
@@ -42,6 +44,17 @@ defmodule Banter.Accounts.User do
     |> validate_length(:email, max: 160)
     |> unsafe_validate_unique(:email, Banter.Repo)
     |> unique_constraint(:email)
+  end
+
+  defp validate_username(changeset) do
+    changeset
+    |> validate_required([:username])
+    |> validate_length(:username, min: 3, max: 25)
+    |> validate_format(:username, ~r/^(?![_])[a-zA-Z_0-9]+(?<!_)$/,
+      message: "must contain only alphanumerics and underscores"
+    )
+    |> unsafe_validate_unique(:username, Banter.Repo)
+    |> unique_constraint(:username)
   end
 
   defp validate_password(changeset, opts) do
