fa82ed65e926f480aaeb7ada9296faab6879a8f5
Ben <lolitsaburrito@gmail.com>
2022-07-09 11:56:01 -0400

    fix indentation, pass on New Post button, WIP click-post-to-expand body, post body limited to 40vh

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,15 +22,25 @@ import "../css/app.css"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+let Hooks = {}
+
+// Copy/pasted from https://github.com/phoenixframework/phoenix_live_view/issues/1011. Maintains textarea size
+Hooks.MaintainAttrs = {
+  attrs () { return this.el.getAttribute("data-attrs").split(", ") },
+  beforeUpdate () { this.prevAttrs = this.attrs().map(name => [name, this.el.getAttribute(name)]) },
+  updated () { this.prevAttrs.forEach(([name, val]) => this.el.setAttribute(name, val)) }
+}
+
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
@@ -42,4 +52,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
